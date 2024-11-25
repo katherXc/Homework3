c76f30417ed5a2b65a8024d337edf2e54b08f0ed
KatherXc <4b3g0068@office.stust.edu.tw>
2024-11-25 21:44:13 +0800

    加入專案檔案。

---
+++ Homework3/Homework3.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <iostream>
+#include "Person.h"
+using namespace std;
+
+int main()
+{
+    Person person1("A123456789", "Chen", "Jason", "M", "1999-01-01");
+
+    Person* person2 = new Person();
+    person2->setId("B987654321");
+    person2->setLastName("Wang");
+    person2->setFirstName("David");
+    person2->setGender("M");
+    person2->setBirthDate("1999-12-31");
+
+    person1.display();
+    cout << endl;
+    person2->display();
+}
